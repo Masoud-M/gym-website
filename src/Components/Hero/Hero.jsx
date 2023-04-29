32cb89e1dbec754efce70c2e636de6134f2a0961
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2023-04-29 13:54:36 +0330

    Initial commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Hero.css";
+import Header from "../Header/Header";
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="left-h">
+        <Header />
+
+        {/* the best ad */}
+        <div className="the-best-ad">
+          <div></div>
+          <span>The best fitness club in the town</span>
+        </div>
+
+        {/* hero heading */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal Body </span>
+          </div>
+          <div>
+            <span>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
+          </div>
+        </div>
+
+        {/* figures */}
+      </div>
+      <div className="right-h">right side</div>
+    </div>
+  );
+};
+
+export default Hero;
