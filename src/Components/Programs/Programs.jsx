dd258459e01115b17273eccbd7c8e60b94f406d3
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2023-05-01 11:49:46 +0330

    added programs category

---
+++ src/Components/Programs/Programs.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Programs.css";
+import { programsData } from "../../data/programsData";
+import RightArrow from "../../assets/rightArrow.png";
+
+const Programs = () => {
+  return (
+    <div className="Programs" id="programs">
+      {/* header */}
+      <div className="programs-header">
+        <span className="stroke-text">explore our</span>
+        <span>programs</span>
+        <span className="stroke-text">to shape you</span>
+      </div>
+
+      {/* program categories */}
+      <div className="programs-categories">
+        {programsData.map((program) => (
+          <div className="category">
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+            <div className="join-now">
+              <span>Join Now</span>
+              <img src={RightArrow} alt="/" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Programs;
