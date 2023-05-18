2ec994f832917e4cde9b72e59fe936fc84993a5e
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2023-05-18 13:10:01 +0330

    Revert "moved the assets from src to public to fix the deploying to the netlify error"
    This reverts commit e82fce056253ae416048fe540284c38133af4d06.

--- src/Components/Reasons/Reasons.jsx
+++ src/Components/Reasons/Reasons.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Reasons.css";
-import image1 from "../../../public/assets/image1.png";
-import image2 from "../../../public/assets/image2.png";
-import image3 from "../../../public/assets/image3.png";
-import image4 from "../../../public/assets/image4.png";
-import nb from "../../../public/assets/nb.png";
-import adidas from "../../../public/assets/adidas.png";
-import nike from "../../../public/assets/nike.png";
-import tick from "../../../public/assets/tick.png";
+import image1 from "../../assets/image1.png";
+import image2 from "../../assets/image2.png";
+import image3 from "../../assets/image3.png";
+import image4 from "../../assets/image4.png";
+import nb from "../../assets/nb.png";
+import adidas from "../../assets/adidas.png";
+import nike from "../../assets/nike.png";
+import tick from "../../assets/tick.png";
 const Reasons = () => {
   return (
     <div className="Reasons" id="reasons">
